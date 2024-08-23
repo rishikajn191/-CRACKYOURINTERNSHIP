e80855bb4d239551aa4b0a9553513882f8960adf
Rishika Jain <122612502+rishikajn191@users.noreply.github.com>
2024-08-23 17:04:41 +0530

    Create Valid_parentheses.cpp

---
+++ Valid_parentheses.cpp
@@ -0,0 +1,18 @@
+// Using STACK i.e. if you see a open bracket then push it into stack otherwise pop if you see correct open bracket corresponding to closed bracket.
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                st.push(s[i]);
+            else {
+                if( st.empty() || (s[i]==']' && st.top()!='[') || (s[i]=='}' && st.top()!='{') || (s[i]==')' && st.top()!='(') )
+                    return false;
+                st.pop();
+            }
+            
+        }
+        return st.empty();
+    }
+};
