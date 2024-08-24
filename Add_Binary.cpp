9d40915de5808dea8ce2a1362417c8322207cd29
Rishika Jain <122612502+rishikajn191@users.noreply.github.com>
2024-08-24 08:57:25 +0530

    Update Add_Binary.cpp

--- Add_Binary.cpp
+++ Add_Binary.cpp
@@ -1,131 +1,69 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n=a.length(), m=b.length();
-        string ans;
-        string tmp;
-        string carry="0";
-        if(n!=m){
-            if(n>m){
-                for(int i=0; i<n-m; i++){
-                    tmp+='0';
-                }
-                tmp+=b;
+        string ans = ""; 
+        string carry = "0";
 
-                for(int i=n-1; i>=0; i--){
-                    if(a[i]==tmp[i] && a[i]==0){
-                        if(carry=="0")
-                            ans+='0';
-                        else
-                            ans+='1';
-                    }
-                
-                    else if(a[i]==tmp[i] && a[i]==1){
-                        if(carry=="0")
-                            ans+='0';
-                        else
-                            ans+='1';
-                        carry="1";
-                    }
-                    else if(a[i]=='0' && tmp[i]=='1'){
-                        if(carry=="0")
-                            ans+='1';
-                        else{
-                            ans+='0';
-                            carry="1";
-                        }
-                    }
+        //  adding leading zeros
+        int n = a.length();
+        int m = b.length();
+        if (n < m)
+            a.insert(0, m-n, '0');
+        else if (m < n)
+            b.insert(0, n-m, '0');
 
-                    else if(a[i]=='1' && tmp[i]=='0'){
-                        if(carry=="0")
-                            ans+='1';
-                        else{
-                            ans+='0';
-                            carry="1";
-                        }
-                    }
-                }
+        cout<<"a= "<<a<<" b="<<b;
+        if(n<m)
+            n=m;
+        else
+            m=n;
+
+        int i=n-1;
+        while(i>=0){
+            if(a[i]==b[i] && a[i]=='0'){
+                //0+ (0+0)
+                if(carry=="0")
+                    ans+='0';
+                //1+ (0+0)
+                else{
+                    ans+='1';
+                    carry="0";
+                }    
             }
-            else{
-                for(int i=0; i<m-n; i++){
-                    tmp+='0';
-                }
-                tmp+=a;
-
-                for(int i=n-1; i>=0; i--){
-                    if(b[i]==tmp[i] && tmp[i]==0){
-                        if(carry=="0")
-                            ans+='0';
-                        else
-                            ans+='1';
-                    }
-                
-                    else if(b[i]==tmp[i] && tmp[i]==1){
-                        if(carry=="0")
-                            ans+='0';
-                        else
-                            ans+='1';
-                        carry="1";
-                    }
-                    else if(tmp[i]=='0' && b[i]=='1'){
-                        if(carry=="0")
-                            ans+='1';
-                        else{
-                            ans+='0';
-                            carry="1";
-                        }
-                    }
-
-                    else if(tmp[i]=='1' && b[i]=='0'){
-                        if(carry=="0")
-                            ans+='1';
-                        else{
-                            ans+='0';
-                            carry="1";
-                        }
-                    }
-                }
-            }
-        }
-        else{
-            for(int i=n-1; i>=0; i--){
-                if(a[i]==b[i] && a[i]==0){
-                    if(carry=="0")
-                        ans+='0';
-                    else
-                        ans+='1';
-                }
-                
-                else if(a[i]==b[i] && a[i]==1){
-                    if(carry=="0")
-                        ans+='0';
-                    else
-                        ans+='1';
+            else if(a[i]==b[i] && a[i]=='1'){
+                //0+ (1+1)
+                if(carry=="0"){
+                    ans+='0';
                     carry="1";
-                }
-
-                else if(a[i]=='0' && b[i]=='1'){
-                    if(carry=="0")
-                        ans+='1';
-                    else{
-                        ans+='0';
-                        carry="1";
-                    }
-                }
-
-                else if(a[i]=='1' && b[i]=='0'){
-                    if(carry=="0")
-                        ans+='1';
-                    else{
-                        ans+='0';
-                        carry="1";
-                    }
-                }
+                } 
+                //1+ (1+1)  
+                else
+                    ans+='1';
             }
+            else if(a[i]=='1' && b[i]=='0'){
+                // 0 +(1+0)
+                if(carry=="0")
+                    ans+='1';
+                // 1 +(1+0)    
+                else
+                    ans+='0';
+            }
+            else if(a[i]=='0' && b[i]=='1'){
+                // 0 +(0+1)
+                if(carry=="0")
+                    ans+='1';
+                // 1 +(0+1)
+                else
+                    ans+='0';
+            }
+            if(i==0)
+                break;
+            i--;
         }
-        
-        ans+=carry;
-        reverse(ans.begin(), ans.end());
+    
+        if(carry=="1")
+            ans+=carry;
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
