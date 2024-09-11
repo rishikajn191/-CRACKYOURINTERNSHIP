ea9c24cc822a44b7e1c7bd3467f145d0676959fe
Rishika Jain <122612502+rishikajn191@users.noreply.github.com>
2024-09-11 16:18:11 +0530

    Update Merge_two_sortedList.cpp

--- Merge_two_sortedList.cpp
+++ Merge_two_sortedList.cpp
@@ -57,25 +57,9 @@ public:
 
 
 // --------------------------------------------Solution2--------------------------------------------
-// /**
-//  * Definition for singly-linked list.
-//  * struct ListNode {
-//  *     int val;
-//  *     ListNode *next;
-//  *     ListNode() : val(0), next(nullptr) {}
-//  *     ListNode(int x) : val(x), next(nullptr) {}
-//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-//  * };
-//  */
 // class Solution {
 // public:
 //     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-//         if(list1==NULL && list2!=NULL)
-//             return list2;
-//         if(list1!=NULL && list2==NULL)
-//             return list1;
-//         if(list1==NULL && list2==NULL)
-//             return list1;
 
 //         ListNode* t1=list1;
 //         ListNode* t2=list2;
