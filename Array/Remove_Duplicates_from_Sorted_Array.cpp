d7ed2cf2078dd40c5203612ffccb12b7715297fb
Rishika Jain <rishika.jn191@gmail.com>
2024-10-09 16:54:10 +0530

    Update Remove_Duplicates_from_Sorted_Array.cpp

--- Array/Remove_Duplicates_from_Sorted_Array.cpp
+++ Array/Remove_Duplicates_from_Sorted_Array.cpp
@@ -1,19 +1,40 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // int k=nums.size();
-        int i=0, j=1;
-        while(j<nums.size()){
-            if(nums[i]==nums[j]){
-                j++;
+        int count=0;
+        for(int i=0; i<nums.size(); i++){
+            if(i==nums.size()-1){
+                break;
             }
-            else{
-                // i++;
-                nums[++i]=nums[j++];
-                // j++;
-                // k++;
+            if(nums[i]!=nums[i+1]){
+                count++;
+                nums[count]= nums[i+1];
             }
         }
-        return i+1;
+        // for(int i=count+1; i<nums.size(); i++){
+        //     nums.pop_back();
+        // }
+        return count+1;
     }
 };
+
+//------------------------------------------------Solution 2------------------------------------------------
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         // int k=nums.size();
+//         int i=0, j=1;
+//         while(j<nums.size()){
+//             if(nums[i]==nums[j]){
+//                 j++;
+//             }
+//             else{
+//                 // i++;
+//                 nums[++i]=nums[j++];
+//                 // j++;
+//                 // k++;
+//             }
+//         }
+//         return i+1;
+//     }
+// };
